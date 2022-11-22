0098d182204f3ec3262d6dad54f927b707c8f086
Andrew Terekhov <andrew_terekhov@yahoo.com>
2022-11-22 10:19:24 -0800

    Refactor shoes creation; different style name

--- C++/iter.cpp
+++ C++/iter.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fmt/format.h>
 
 struct Shoe {
     Shoe(int s, std::string const& st) : size(s), style(st) {}
@@ -19,6 +20,26 @@ void shoes_in_size(Shoes& shoes, int size) {
     shoes.erase(std::remove_if(begin(shoes), end(shoes), [size](Shoe const& s) { return s.size != size; }), end(shoes));
 }
 
+Shoes shoesFactory(int num)
+{
+    Shoes shoes;
+    for (int i = 0; i < num; ++i) {
+        static char const* const styles[] = {
+            "boot",
+            "sneaker",
+            "sandal",
+            "pump",
+            "highheel"
+        };
+        int size = i % 12;
+        char const* style = styles[i % 5];
+        // very slow snprintf(buf, sizeof(buf), "%s%d", style, size);
+        // slow shoes.push_back(Shoe(size, style + fmt::format("{}", size)));
+        shoes.push_back(Shoe(size, style + std::to_string(i)));
+    }
+    return shoes;
+}
+
 int main(int argc, char* argv[])
 {
     int loops = 1000;
@@ -29,20 +50,7 @@ int main(int argc, char* argv[])
     int counter = 0;
     int shoes_num = 200;
     for (int l = 0; l < loops; ++l) {
-        Shoes shoes;
-        for (int i = 0; i < shoes_num; ++i) {
-            static char const* const styles[] = {
-                "boot",
-                "sneaker",
-                "sandal",
-                "pump",
-                "highheel"
-            };
-
-            int size = i % 12;
-            char const* style = styles[i % 5];
-            shoes.push_back(Shoe(size, style + std::to_string(size)));
-        }
+        Shoes shoes = shoesFactory(shoes_num);
         shoes_in_size(shoes, 10);
         if (l == 0) {
             for (auto const& shoe : shoes) {
