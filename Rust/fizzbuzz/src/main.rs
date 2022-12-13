99c0c8ed7d7c443cbe60f80ec57a918cd5717359
Andrew Terekhov <andrew_terekhov@yahoo.com>
2022-12-13 15:41:01 -0800

    More improvements

--- Rust/fizzbuzz/src/main.rs
+++ Rust/fizzbuzz/src/main.rs
@@ -1,10 +1,18 @@
 use std::env;
+use std::borrow::Cow;
 
 fn fizzbuzz(i: i32) -> String {
     if i % 15 == 0 { String::from("FizzBuzz") } 
     else if i % 5 == 0 { String::from("Buzz") } 
     else if i % 3 == 0 { String::from("Fizz") } 
     else { i.to_string() }
+}
+
+fn fizzbuzz2(i: i32) -> Cow<'static, str> {
+    if i % 15 == 0 { "FizzBuzz".into() } 
+    else if i % 5 == 0 { "Buzz".into() } 
+    else if i % 3 == 0 { "Fizz".into() } 
+    else { i.to_string().into() }
 }
 
 fn main() {
@@ -16,7 +24,8 @@ fn main() {
     let mut l = 0;
     let mut sumlen = 0;
     while l < loops {
-        let res = fizzbuzz(l);
+        //let res = fizzbuzz(l);
+        let res = fizzbuzz2(l);
         sumlen += res.len();
         l += 1;
     }
