e219427da7cdd03cce20476c35591a7cfc239456
Andrew Terekhov <andrew_terekhov@yahoo.com>
2023-02-28 20:23:05 -0800

    Added ShiftOr algo by Kolya

--- C++/load_file_part.cpp
+++ C++/load_file_part.cpp
@@ -29,15 +29,55 @@ size_t loadPatterns(std::string const& fname, Patterns& toFind)
     return maxlen;
 }
 
+int index(const char* text, const char* pattern) {
+    if (!text || !*text) { return -1; }
+    unsigned long state = 0;
+    unsigned long mask [256] = {0};
+    int patlen = strlen(pattern);
+    // Pattern is too short or too long
+    if (patlen == 0 || patlen > 63) {
+        return -1;
+    }
+    // Initialize mask table for each letter in the pattern
+    for (int i = 0; i < patlen; i++) {
+	    unsigned int ind = pattern[i] & 0xff;
+        mask[ind] = mask[ind] | (1 << i);
+    }
+    int textlen = strlen(text);
+    for (int i = 0; i < textlen; i++) {
+        // Update state by shifting it and masking with the record from table
+	    unsigned int ind = text[i] & 0xff; 
+        state = (state << 1) + 1;
+        state = state & mask[ind];
+        if ((state & (1 << (patlen - 1))) != 0) {
+            // It's a match!
+            return (i - patlen + 1);
+        }
+    }
+    // No match found
+    return -1;
+}
+
 size_t search(std::string const& buf, std::string const& pat)
 {
     size_t found = 0;
+    // Regular standard search
     char const* pos = buf.c_str();
     while ((pos = strstr(pos, pat.c_str())) != nullptr)
     {
         ++found;
         ++pos;
     }
+    // ShiftOr search found and implemented by Kolya
+    /*
+    int ind;
+    char const* p = buf.c_str();
+    while ((ind = index(p, pat.c_str())) != -1)
+    {
+        ++found;
+        p += (ind + 1);
+    }
+    */
     return found;
 }
 
