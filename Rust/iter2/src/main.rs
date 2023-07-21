c375a103e48158a87d09b79da411cd70a08fac83
Andrew Terekhov <andrew_terekhov@yahoo.com>
2023-07-21 15:17:46 -0700

    Capitalize first symbol with iterators

--- Rust/iter2/src/main.rs
+++ Rust/iter2/src/main.rs
@@ -1,6 +1,22 @@
+// Function to capitalize the first letter in string slice
+// It is not trivial because strings are UTF-8
+pub fn capitalize_first(w: &str) -> String {
+    let mut c = w.chars();
+    match c.next() {
+        None => String::new(),
+        Some(first) => first.to_uppercase().chain(c).collect(),
+    }
+}
+
 fn main() {
     let s = &["foo", "bar"];
     println!("s: {:?}", s);
     let v: Vec<String> = s.iter().map(|s| s.to_string().to_uppercase()).collect();
     println!("v: {:?}", v);
+
+    let v = vec!["hello", " ", "world"];
+    // Cannot write as 1 expression because type Vec<String> cannot be auto determined
+    let v2: Vec<String> = v.iter().map(|w| capitalize_first(w)).collect();
+    let s = v2.concat();
+    println!("s: {:?}", s)
 }
