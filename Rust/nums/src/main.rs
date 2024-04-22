ad845e28cb57ac949ab8b7ea6ee22a0467a71334
Andrew Terekhov <andrew_terekhov@yahoo.com>
2024-04-22 12:44:07 -0700

    new changes for rust

---
+++ Rust/nums/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Hello, nums!");
+
+    let fl = [1.1, 2.2, 3.3];
+    let mut i = 0;
+    for f in fl {
+        println!("i: {}, f: {}", i, f);
+        i += 1;
+    }
+
+    println!("fl: {:?}", fl);
+
+    let nums = vec!(1, 2, 3);
+    for n in nums {
+        println!("n: {}", n);
+    }
+
+    /* nums moved because of implicit iter_into
+    for n in nums {
+        println!("n: {}", n);
+    } */
+}
