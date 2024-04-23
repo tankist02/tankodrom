96ffe1b949e01cb5bfb06713ba68c6f7f6404e16
Andrew Terekhov <andrew_terekhov@yahoo.com>
2024-04-23 16:55:36 -0700

    Various methods of getting current time in Rust and C++

--- C++/gettime.cpp
+++ C++/gettime.cpp
@@ -1,26 +1,44 @@
 #include <cstdio>
 #include <ctime>
+#include <string>
+#include <iostream>
+
+//#define CLOCK_TYPE CLOCK_REALTIME
+//#define CLOCK_TYPE CLOCK_REALTIME_COARSE
 
 inline long millis(struct timespec const& start, struct timespec const& stop)
 {
     return (stop.tv_sec - start.tv_sec) * 1000 + (stop.tv_nsec - start.tv_nsec) / 1000000;
 }
 
-int main(int argc, char* argv[])
+std::string prettyPrint(long value)
+{
+    std::string res = std::to_string(value);
+    for (int i = res.size() - 3; i > 0; i -= 3) { res.insert(i, ","); }
+    return res;
+}
+
+void process(int clock_type, char const* msg)
 {
     long count = 0;
     struct timespec start, stop;
 
-    clock_gettime(CLOCK_REALTIME, &start);
+    clock_gettime(clock_type, &start);
     long ms = 0;
 
     while (ms < 1000)
     {
         ++count;
-        clock_gettime(CLOCK_REALTIME, &stop);
+        clock_gettime(clock_type, &stop);
         ms = millis(start, stop);
     }
 
-    printf("count: %ld\n", count);
+    std::cout << msg << ": " << prettyPrint(count) << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    process(CLOCK_REALTIME, "realtime");
+    process(CLOCK_REALTIME_COARSE, "realtime");
 }
 
--- Rust/time/src/main.rs
+++ Rust/time/src/main.rs
@@ -1,5 +1,38 @@
+use libc;
 use std::time::{Duration, Instant};
 use thousands::Separable;
+
+fn now_native(clock_type: i32) -> (i64, i64) {
+    let mut time = libc::timespec {
+        tv_sec: 0,
+        tv_nsec: 0,
+    };
+
+    //let ret = unsafe { libc::clock_gettime(libc::CLOCK_REALTIME, &mut time) };
+    //let ret = unsafe { libc::clock_gettime(libc::CLOCK_REALTIME_COARSE, &mut time) };
+    let ret = unsafe { libc::clock_gettime(clock_type, &mut time) };
+    assert_eq!(ret, 0);
+    (time.tv_sec, time.tv_nsec)
+}
+
+fn millis(start: (i64, i64), stop: (i64, i64)) -> i64
+{
+    (stop.0 - start.0) * 1000 + (stop.1 - start.1) / 1000000
+}
+
+fn process_native(clock_type: i32, msg: &str) -> () {
+    let start = now_native(clock_type);
+
+    let mut ms: i64 = 0;
+    let mut count = 0;
+
+    while ms < 1000 {
+        count += 1;
+        let stop = now_native(clock_type);
+        ms = millis(start, stop);
+    }
+    println!("{}: {}", msg, count.separate_with_commas());
+}
 
 fn main() {
     println!("Hello, time!");
@@ -10,5 +43,8 @@ fn main() {
     while Instant::now() - start < second {
         count += 1;
     }
-    println!("count: {}", count.separate_with_commas());
+    println!("Rust: {}", count.separate_with_commas());
+
+    process_native(libc::CLOCK_REALTIME, "realtime");
+    process_native(libc::CLOCK_REALTIME_COARSE, "realtimecoarse");
 }
