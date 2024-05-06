37a3440f46a1508ea71a762d59411ee19da05f6a
Andrew Terekhov <andrew_terekhov@yahoo.com>
2024-05-05 17:54:10 -0700

    More Rust samples

--- Rust/file/src/main.rs
+++ Rust/file/src/main.rs
@@ -23,5 +23,59 @@ fn main() {
         },
     };
     println!("Loaded file: {}, content: {}", n, s);
-}
 
+    let buf: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let text = String::from_utf8_lossy(&buf);
+    println!("buf: {:?}, text: {}", buf, text);
+
+    let mut x = 0;
+    let y = &x;
+    //x = 1;
+    println!("x: {}, y: {}", x, y);
+    //println!("x: {}", x);
+
+    let mut configs: Vec<String> = vec![
+        String::from("foo"),
+        String::from("bar"),
+    ];
+
+    let mut baz: Vec<String> = Vec::new();
+    let mut bazr: Vec<&str> = Vec::new();
+    //baz.push(configs[1]);
+    //baz.push(configs.get(1).unwrap());
+    baz.push(configs[1].clone());
+    println!("configs: {:?}, baz: {:?}", configs, baz);
+    bazr.push(&configs[0]);
+    bazr.push(&configs[1]);
+    println!("bazr: {:?}", bazr);
+
+    //configs[0] = String::from("foobar");
+    //println!("configs: {:?}, bazr: {:?}", configs, bazr);
+
+    #[derive(Debug)]
+    struct File {
+        name: String,
+        data: Vec<u8>,
+    }
+
+    impl File {
+        fn new(n: &str) -> File {
+            File {
+                name: String::from(n),
+                data: Vec::new(),
+            }
+        }
+
+        fn new_data(n: &str, d: Vec<u8>) -> File {
+            File {
+                name: String::from(n),
+                data: d,          
+            }
+        }
+    }
+
+    let f1 = File::new("foo");
+    let data = vec![1, 2];
+    let f2 = File::new_data("bar", data);
+    println!("f1: {:?}, f2: {:?}", f1, f2);
+}
