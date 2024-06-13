a96d5ec3dc6bd28892671002b389b7f0944de510
Andrew Terekhov <andrew_terekhov@yahoo.com>
2024-06-12 17:13:21 -0700

    Little fixes for Rust style

--- Rust/alphabet/src/main.rs
+++ Rust/alphabet/src/main.rs
@@ -4,22 +4,22 @@ fn main() {
     println!("Hello, alphabet!");
 
     let text = "the quick brown fox jumps over the lazy dog";
-    const size: usize = 'z' as usize - 'a' as usize + 1;
+    const SIZE: usize = 'z' as usize - 'a' as usize + 1;
 
     let now = Instant::now();
 
     let loops = 100_000_000;
     let mut sum = 0;
 
-    for l in 0..loops {
-        let mut counts = [0; size];
+    for _ in 0..loops {
+        let mut counts = [0; SIZE];
         for c in text.chars() {
             if c != ' ' {
                 let index = c as usize - 'a' as usize;
                 counts[index] += 1;
             }
         }
-        sum += counts[size - 1];
+        sum += counts[SIZE - 1];
     }
     println!("sum: {}, elapsed: {:?}", sum, now.elapsed());
     //println!("counts: {:?}", counts);
