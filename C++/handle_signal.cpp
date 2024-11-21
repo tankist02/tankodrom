c65dc44ea0032409376f418751c41706127447bb
Andrew Terekhov <andrew_terekhov@yahoo.com>
2024-11-20 21:51:59 -0800

    Add libunwind to print symbols names

--- C++/handle_signal.cpp
+++ C++/handle_signal.cpp
@@ -1,5 +1,9 @@
 #include <execinfo.h>  // For backtrace()
-#include <fcntl.h>     // for open()
+
+#include <cxxabi.h>
+
+#define UNW_LOCAL_ONLY
+#include <libunwind.h>
 
 #include <cassert>
 #include <chrono>
@@ -10,7 +14,47 @@ static constexpr size_t MAX_STACKTRACE_DEPTH = 64;
 
 std::string globMessage;
 
-void writeStackTrace(int file_descriptor) 
+void backtrace()
+{
+	unw_cursor_t cursor;
+	unw_context_t context;
+
+	// Initialize cursor to current frame for local unwinding.
+	unw_getcontext(&context);
+	unw_init_local(&cursor, &context);
+
+	// Unwind frames one by one, going up the frame stack.
+	while (unw_step(&cursor) > 0)
+	{
+		unw_word_t offset, pc;
+		unw_get_reg(&cursor, UNW_REG_IP, &pc);
+		if (pc == 0)
+		{
+			break;
+		}
+		//printf("0x%lx:", pc);
+
+		char sym[256];
+		if (unw_get_proc_name(&cursor, sym, sizeof(sym), &offset) == 0)
+        {
+			//printf(" (%s+0x%lx)\n", sym, offset);
+
+            int status;
+            char* name = abi::__cxa_demangle(sym, nullptr, nullptr, &status);
+            if (status == 0)
+            {
+                printf("symbol: %s (%s+0x%lx)\n", name, sym, offset);
+                free(name);
+            }
+		}
+        else
+        {
+			//printf(" -- error: unable to obtain symbol name for this frame\n");
+		}
+	}
+}
+
+/*void writeStackTrace(int file_descriptor)
 {
 	void* trace[MAX_STACKTRACE_DEPTH];
 	size_t trace_depth = backtrace(trace, MAX_STACKTRACE_DEPTH);
@@ -18,10 +62,10 @@ void writeStackTrace(int file_descriptor)
 	// the printed trace.
 	backtrace_symbols_fd(trace + 1, trace_depth - 1, file_descriptor);
 	//backtrace_symbols_fd(trace, trace_depth, file_descriptor);
-}
+}*/
 
 /******************************************************************************/
-void fatalSignalHandler(int signal) 
+void fatalSignalHandler(int signal)
 {
 	// Restore the default signal handler for SIGSEGV in case another one
 	// happens, and for the re-issue below.
@@ -31,9 +75,11 @@ void fatalSignalHandler(int signal)
 	static constexpr const char error_msg[] =
 		"*** fatalSignalHandler stack trace: ***\n";
 	// Write using safe `write` function. Skip null character.
-	if (write(fileno(stderr), error_msg, sizeof(error_msg) - 1) > 0) 
+	if (write(fileno(stderr), error_msg, sizeof(error_msg) - 1) > 0)
 	{
-		writeStackTrace(fileno(stderr));
+        backtrace();
+
+		//writeStackTrace(fileno(stderr));
 	    write(fileno(stderr), globMessage.c_str(), globMessage.size());
 	    write(fileno(stderr), "\n", 1);
 	}
@@ -61,7 +107,7 @@ void func3()
     func2();
 }
 
-int main() 
+int main()
 {
 	// Use our function to handle segmentation faults.
 	// Could add additional signals like: SIGSEGV, SIGSYS, etc.
